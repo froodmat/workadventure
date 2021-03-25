d57afafc908cd62d19d258f3b8689a44b06e3126
froodmat <froodmat@gmail.com>
2021-03-25 15:35:37 +0100

    tilesets aktualisieren

---
+++ pH64_plants_OGA_alpha.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="pH64_plants_OGA_alpha" tilewidth="32" tileheight="32" tilecount="84" columns="14">
+ <image source="../../../Dropbox/WorkAdventure/Assets/Detail_Sprites/pH64_plants_OGA_alpha.png" width="448" height="192"/>
+</tileset>
---
+++ rpg_maker_vx__modernrtp_tilea2_by_painhurt_d3f7rwg.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="rpg_maker_vx__modernrtp_tilea2_by_painhurt_d3f7rwg" tilewidth="32" tileheight="32" tilecount="192" columns="16">
+ <image source="../../../Dropbox/WorkAdventure/Assets/Discohütte (AcidSmiliey)/rpg_maker_vx__modernrtp_tilea2_by_painhurt_d3f7rwg.png" width="512" height="384"/>
+</tileset>
